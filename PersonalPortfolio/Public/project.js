ac4af8d588d5a1314419d79a5890c6403a9e7bd8
maxsdrv <maxim_sdrv@mail.ru>
2022-10-21 15:57:50 +0300

    bug nodemailer... incorrect response

---
+++ PersonalPortfolio/Public/project.js
@@ -0,0 +1,22 @@
+let projects = [
+    {
+        name: "project one",
+        tags: "#c++, #network, #game",
+        image: "Selection_001.png",
+    },
+    {
+        name: "project two",
+        tags: "#c++, #network" ,
+        image: "Selection_002.png",
+    },
+    {
+        name: "project three",
+        tags: "#c++, #game",
+        image: "Selection_003.png",
+    },
+    {
+        name: "project four",
+        tags: "#c++, #ui",
+        image: "Selection_004.png",
+    }
+]
--- PersonalPortfolio/server.js
+++ PersonalPortfolio/server.js
@@ -14,6 +14,35 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(initialPath, "index.html"));
 })
 
+app.post('/mail', (req, res) => {
+    const { firstname, lastname, email, msg } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    })
+
+    const mailOptions = {
+        from: 'makscch@gmail.com',
+        to: 'makscch@gmail.com',
+        subject: 'Postfolio',
+        text: `First name: ${firstname}, \nLast name: ${lastname}, \nEmail: ${email}, \nMessage: ${msg}`
+    }
+
+    transporter.sendMail(mailOptions, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json('oops! it seems like some error occurred please try again.');
+        }
+        else {
+            res.json('thanks for e-mailing me. I will reply to you within 2 working days.');
+        }
+    })
+})
+
 app.listen(3000, () => {
     console.log('listening......');
 })
